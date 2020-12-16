5729980036d67a5eae37863d60dad60380550afa
Laurent85 <manceaulaurent@yahoo.fr>
2020-12-16 16:54:02 +0100

    ok

--- Constitution des classes/Form1.Designer.cs
+++ Constitution des classes/Form1.Designer.cs
@@ -33,11 +33,15 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.Configuration = new System.Windows.Forms.TabPage();
+            this.tabControl.SuspendLayout();
+            this.Configuration.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(95, 47);
+            this.button1.Location = new System.Drawing.Point(6, 20);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(116, 23);
             this.button1.TabIndex = 0;
@@ -48,7 +52,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(101, 100);
+            this.label1.Location = new System.Drawing.Point(12, 66);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(110, 13);
             this.label1.TabIndex = 1;
@@ -56,7 +60,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(230, 97);
+            this.textBox1.Location = new System.Drawing.Point(146, 63);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(57, 20);
             this.textBox1.TabIndex = 2;
@@ -64,7 +68,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(227, 52);
+            this.label2.Location = new System.Drawing.Point(143, 30);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(35, 13);
             this.label2.TabIndex = 3;
@@ -72,7 +76,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(95, 154);
+            this.button2.Location = new System.Drawing.Point(15, 111);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 4;
@@ -80,21 +84,44 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // tabControl
+            // 
+            this.tabControl.Controls.Add(this.Configuration);
+            this.tabControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl.Location = new System.Drawing.Point(0, 0);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(1380, 591);
+            this.tabControl.TabIndex = 5;
+            // 
+            // Configuration
+            // 
+            this.Configuration.Controls.Add(this.button1);
+            this.Configuration.Controls.Add(this.button2);
+            this.Configuration.Controls.Add(this.label2);
+            this.Configuration.Controls.Add(this.textBox1);
+            this.Configuration.Controls.Add(this.label1);
+            this.Configuration.Location = new System.Drawing.Point(4, 22);
+            this.Configuration.Name = "Configuration";
+            this.Configuration.Padding = new System.Windows.Forms.Padding(3);
+            this.Configuration.Size = new System.Drawing.Size(1372, 565);
+            this.Configuration.TabIndex = 0;
+            this.Configuration.Text = "Configuration";
+            this.Configuration.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1380, 591);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tabControl);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl.ResumeLayout(false);
+            this.Configuration.ResumeLayout(false);
+            this.Configuration.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -105,6 +132,8 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.TabPage Configuration;
     }
 }
 
--- Constitution des classes/Form1.cs
+++ Constitution des classes/Form1.cs
@@ -51,8 +51,7 @@ namespace Constitution_des_classes
 
         private void button2_Click(object sender, EventArgs e)
         {
-            int compteurBulletins = 0;
-            char Classe = 'A';
+            char classe = 'A';
             var excelApplication = new Microsoft.Office.Interop.Excel.Application();
 
             var fichierEcolesXlsx = excelApplication.Workbooks.Open(label2.Text);
@@ -62,26 +61,162 @@ namespace Constitution_des_classes
                 XlSearchOrder.xlByRows, XlSearchDirection.xlPrevious,
                 false, System.Reflection.Missing.Value, System.Reflection.Missing.Value).Row;
             var range = feuilleEcoles.Range["B5:B" + dernierRang];
-            TabControl tabControl = new TabControl();
+            //TabControl tabControl = new TabControl();
             tabControl.Dock = DockStyle.Fill;
             TabPage tp1 = new TabPage("Tous les élèves");
             tabControl.TabPages.Add(tp1);
-            this.Controls.Add(tabControl);
-            if (range[5, 1].Text.Contains("6"))
+            Controls.Add(tabControl);
+            string division = range[5, 1].Text.Substring(0, 1);
+            //if (range[5, 1].Text.Contains("6"))
             {
                 for (int i = 1; i <= Int16.Parse(textBox1.Text); i++)
                 {
-                    TabPage tp = new TabPage("6" + Classe);
+                    TabPage tp = new TabPage(division + classe);
                     tabControl.TabPages.Add(tp);
 
                     //tp.Controls.Add(new System.Windows.Forms.Button());
                     //this.Controls.Add(tabControl);
-                    Classe++;
+                    classe++;
                 }
             }
-            TabPage t = tabControl.TabPages[2];
-            tabControl.SelectedTab = t; //go to tab 
-            t.Controls.Add(new System.Windows.Forms.Button());
+            TabPage t = tabControl.TabPages[1];
+            //tabControl.SelectedTab = t; //go to tab 
+            ListView liste = new ListView();
+            liste.Dock = DockStyle.Fill;
+            liste.LabelEdit = true;
+            // Allow the user to rearrange columns.
+            liste.AllowColumnReorder = true;
+            // Display check boxes.
+            liste.CheckBoxes = false;
+            // Select the item and subitems when selection is made.
+            liste.FullRowSelect = true;
+            // Display grid lines.
+            liste.GridLines = true;
+            // Sort the items in the list in ascending order.
+            liste.Sorting = SortOrder.Ascending;
+            liste.Scrollable = true;
+            // Set to details view.
+            liste.View = View.Details;
+            
+
+
+            t.Controls.Add(liste);
+
+            var range1 = feuilleEcoles.Range["A5:G" + dernierRang];
+            int rowCount = range1.Rows.Count;
+            int colCount = range1.Columns.Count;
+            ListViewItem itm = new ListViewItem();
+            string[] arr = new string[7];
+
+            // Add a column with width 20 and left alignment.
+            liste.Columns.Add(range1[0,1].Text, -2, HorizontalAlignment.Left);
+            liste.Columns.Add(range1[0, 2].Text, -2, HorizontalAlignment.Left);
+            liste.Columns.Add(range1[0, 3].Text, -2, HorizontalAlignment.Left);
+            liste.Columns.Add(range1[0, 4].Text, -2, HorizontalAlignment.Left);
+            liste.Columns.Add(range1[0, 5].Text, -2, HorizontalAlignment.Left);
+            liste.Columns.Add(range1[0, 6].Text, -2, HorizontalAlignment.Left);
+            liste.Columns.Add(range1[0, 7].Text, -2, HorizontalAlignment.Left);
+
+
+            for (int i = 4; i <= rowCount; i++)
+            {
+                
+                for (int j = 1; j <= 7; j++)
+                {
+
+
+                    try
+                    {
+
+                        
+                        
+                        ListViewItem lvitem = new ListViewItem();
+                        lvitem.Text = range1.Cells[i, j].Value2.ToString();
+                        arr[j-1] = lvitem.Text;
+                        //lvitem.SubItems.Add(range1.Cells[i, j].Value2.ToString());
+                        //lvitem.SubItems.Add(range1.Cells[i, 3].Value2.ToString());
+                        
+                        
+
+                    }
+                    catch (Exception ex)
+                    {
+                        //MessageBox.Show(ex.Message + "\n" + ex.StackTrace);
+                    }
+
+                    
+                }
+                itm = new ListViewItem(arr);
+                liste.Items.Add(itm);
+            }
+        }
+
+        private void ImportExcel(string filename, ListView liste)
+        {
+            Microsoft.Office.Interop.Excel.Application xla = new Microsoft.Office.Interop.Excel.Application();
+            xla.Visible = true;
+
+            // Load the workbook
+            Microsoft.Office.Interop.Excel.Workbook wb = xla.Workbooks.Open(filename);
+
+            // Get the worksheet
+            Microsoft.Office.Interop.Excel.Worksheet ws = wb.Worksheets[1];
+
+            // Retrieve number of columns, number of rows
+            int numberOfColumns = GetNumberOfColumns(ws);
+            int numberOfRows = GetNumberOfRows(ws);
+
+            for (int r = 1; r <= numberOfRows; r++)
+            {
+                // Put your own code here to add the listviewitem
+                ListViewItem item = liste.Items.Add(ws.Cells[r, 1].Value);
+
+                for (int c = 1; c <= numberOfColumns; c++)
+                {
+                    // Put your own code here to add the subitems
+                    item.SubItems.Add(ws.Cells[r, c].Value.ToString());
+                }
+            }
+
+            // Quit Excel
+            xla.Quit();
+            xla = null;
+        }
+
+        /// <summary>
+        /// Gets the number of rows by searching the first null row
+        /// </summary>
+        /// <param name="ws">worksheet to search</param>
+        /// <returns>number of rows</returns>
+        /// <remarks>there probably is a better way to do this</remarks>
+        private int GetNumberOfRows(Microsoft.Office.Interop.Excel.Worksheet ws)
+        {
+            int numberOfRows = 5;
+            while (ws.Cells[1, numberOfRows].Value != null)
+            {
+                numberOfRows += 1;
+            }
+            numberOfRows -= 1; // substract 1 to get the last filled column
+
+            return numberOfRows;
+        }
+
+        /// <summary>
+        /// Gets the number of columns by searching the first null row
+        /// </summary>
+        /// <param name="ws">worksheet to search</param>
+        /// <returns>number of columns</returns>
+        /// <remarks>there probably is a better way to do this</remarks>
+        private int GetNumberOfColumns(Microsoft.Office.Interop.Excel.Worksheet ws)
+        {
+            int numberOfColumns = 1;
+            while (ws.Cells[1, numberOfColumns].Value != null)
+            {
+                numberOfColumns += 1;
+            }
+            numberOfColumns -= 1; // substract 1 to get the last filled column
+
+            return numberOfColumns;
         }
     }
 }
